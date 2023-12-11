3ef61869b11a3aa2e02e07a406f5eeadb5f67a8d
Paulina <paulina.kuchta-lesniak@student.howest.be>
2023-12-11 21:01:32 +0100

    tickets displayed

---
+++ client/src/scripts/buttonSpeakers.js
@@ -0,0 +1,18 @@
+const $btnCopenhagen = document.querySelector(".button__copenhagen");
+const $btnOslo = document.querySelector(".button__oslo");
+const $listOslo = document.querySelector(".speakers__cards-list--oslo");
+const $listCopenhagen = document.querySelector(
+  ".speakers__cards-list--copenhagen"
+);
+
+const toggleSpeakers = (e) => {
+  const isCopenhagen = e.target === $btnCopenhagen;
+  
+  $listOslo.classList.toggle("hidden-element", isCopenhagen);
+  $listCopenhagen.classList.toggle("hidden-element", !isCopenhagen);
+  $btnOslo.classList.toggle("button__active", !isCopenhagen);
+  $btnCopenhagen.classList.toggle("button__active", isCopenhagen);
+};
+
+$btnCopenhagen.addEventListener("click", toggleSpeakers);
+$btnOslo.addEventListener("click", toggleSpeakers);
