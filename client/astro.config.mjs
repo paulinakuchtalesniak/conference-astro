4fba93ceda6b590375d58c8ca6d4979e4adac647
Paulina <paulina.kuchta-lesniak@student.howest.be>
2023-12-12 14:45:06 +0100

    deployment

--- client/astro.config.mjs
+++ client/astro.config.mjs
@@ -1,3 +1,14 @@
+import { defineConfig } from "astro/config";
+
+// https://astro.build/config
+// export default defineConfig({
+//   site: "https://paulina-kuchta.com",
+//   base: "/astro",
+//   image: {
+//     domains: ["localhost"],
+//   },
+// });
+
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
