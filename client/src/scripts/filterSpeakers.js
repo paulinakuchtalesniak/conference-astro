e1cb96a0153ff16c67be035117ef41da8cfbf256
Paulina <paulina.kuchta-lesniak@student.howest.be>
2023-12-16 17:55:37 +0100

    filter logic added

---
+++ client/src/scripts/filterSpeakers.js
@@ -0,0 +1,56 @@
+const filterState = {
+  tag: null,
+  date: "",
+  locations: [],
+};
+const setupDateTabs = () => {
+  const tabs = document.querySelectorAll(".date-tab");
+  tabs.forEach((tab) => {
+    tab.addEventListener("click", () => {
+      tabs.forEach((t) => t.classList.remove("active"));
+      tab.classList.add("active");
+
+      filterState.date = tab.getAttribute("data-date");
+      applyFilters();
+    });
+  });
+};
+document.querySelectorAll('input[name="locations"]').forEach((checkbox) => {
+  checkbox.addEventListener("change", (event) => {
+    const value = event.target.value;
+    const checked = event.target.checked;
+
+    if (checked) {
+      filterState.locations.push(value);
+    } else {
+      filterState.locations = filterState.locations.filter(
+        (loc) => loc !== value
+      );
+    }
+
+    applyFilters();
+  });
+});
+
+const applyFilters = () => {
+  const speakers = document.querySelectorAll(".speakers__card-lineup");
+
+  speakers.forEach((speaker) => {
+    const speakerDate = speaker.getAttribute("data-date-speaker");
+    const speakerLocation = speaker.getAttribute("data-location");
+    const matchesDate =
+      filterState.date === "" || filterState.date === speakerDate;
+    const matchesLocation =
+      filterState.locations.length === 0 ||
+      filterState.locations.includes(speakerLocation);
+
+    if (matchesDate && matchesLocation) {
+      speaker.classList.remove("hidden");
+    } else {
+      speaker.classList.add("hidden");
+    }
+  });
+};
+
+setupDateTabs();
+applyFilters();
